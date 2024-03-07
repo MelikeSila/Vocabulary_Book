771766f7dba9b5913c26088c2d8d0adedf65f63d
MelikeSila <melikekesskin@gmail.com>
2024-03-07 16:47:18 +0100

    favorite and flip button activated

--- app/src/main/java/com/example/vocabularybook/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/vocabularybook/ui/home/HomeFragment.kt
@@ -12,6 +12,7 @@ import android.view.animation.AccelerateInterpolator
 import android.view.animation.DecelerateInterpolator
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -41,25 +42,7 @@ class HomeFragment : Fragment() {
         /// Flip the card
         val word_text_view = root.findViewById<TextView>(R.id.word_text_view)
         word_text_view.setOnClickListener{
-            val anim_1 = ObjectAnimator.ofFloat(word_text_view, "scaleX", 1f, 0f)
-            val anim_2 = ObjectAnimator.ofFloat(word_text_view, "scaleX", 0f, 1f)
-
-            anim_1.interpolator = DecelerateInterpolator()
-            anim_2.interpolator = AccelerateInterpolator()
-
-            anim_1.addListener(object: AnimatorListenerAdapter(){
-                override fun onAnimationEnd(animation: Animator) {
-                    super.onAnimationEnd(animation)
-                    if ( word_text_view.text == "Front"){
-                        word_text_view.text = "Back"
-                        anim_2.start()
-                    }else{
-                        word_text_view.text = "Front"
-                        anim_2.start()
-                    }
-                }
-            })
-            anim_1.start()
+            flip(word_text_view)
         }
 
         // load data
@@ -67,12 +50,52 @@ class HomeFragment : Fragment() {
         loadData(root)
 
         // save data
-        val save_btn = root.findViewById<Button>(R.id.save_button)
-        save_btn.setOnClickListener{
+        val saveBtn = root.findViewById<Button>(R.id.save_button)
+        saveBtn.setOnClickListener{
             saveData(root)
         }
 
+        val flipBtn = root.findViewById<Button>(R.id.flip_button)
+        flipBtn.setOnClickListener {
+            flip(word_text_view)
+        }
+        var is_saved = false
+        val starBtn = root.findViewById<ImageView>(R.id.imageView2)
+        starBtn.setOnClickListener {
+            //btn_star_big_on
+            if (is_saved){
+                starBtn.setImageResource(com.example.vocabularybook.R.drawable.ic_star_off)
+                is_saved = false
+            }else {
+                starBtn.setImageResource(R.drawable.ic_star_on)
+                is_saved = true
+            }
+
+        }
+
         return root
+    }
+
+    private fun flip(textView: TextView){
+        val anim_1 = ObjectAnimator.ofFloat(textView, "scaleX", 1f, 0f)
+        val anim_2 = ObjectAnimator.ofFloat(textView, "scaleX", 0f, 1f)
+
+        anim_1.interpolator = DecelerateInterpolator()
+        anim_2.interpolator = AccelerateInterpolator()
+
+        anim_1.addListener(object: AnimatorListenerAdapter(){
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                if ( textView.text == "Front"){
+                    textView.text = "Back"
+                    anim_2.start()
+                }else{
+                    textView.text = "Front"
+                    anim_2.start()
+                }
+            }
+        })
+        anim_1.start()
     }
 
     private fun loadData(root: View) {
